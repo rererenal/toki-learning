e73382bb6d5c8cbac8fa3ffcddf5c0667826167f
Rere Renal <renaldi232701@gmail.com>
2020-09-08 21:20:40 +0700

    Solved 'BurungBeo' problem

---
+++ src/BurungBeo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BurungBeo {
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		String a = scanner.nextLine();
+		System.out.println(a);
+	}
+	
+}
