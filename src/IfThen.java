6a5cb0f982baf9f518e12f83be6f3efce77a6118
Rere Renal <renaldi232701@gmail.com>
2020-09-16 13:50:29 +0700

    Solved 'IfThen' problem

---
+++ src/IfThen.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IfThen {
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+	
+		if (a>0) {
+			System.out.println(a);
+		}
+	}
+}
+	
+
